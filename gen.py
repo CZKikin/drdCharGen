d373490c48383b0f955a8947ebe56a4f1ce21436
CZKikin <krystofsadlik13@seznam.cz>
2021-01-16 11:34:11 +0100

    fixed names, generator is now complete

--- gen.py
+++ gen.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 import random
-def vyberZeSouboru(soubor):
+def najdiOpravu(soubor, char):
+    opravy = {}
+    objekty = []
+    with open(soubor, "r") as f:
+        data = f.readlines()
+        keys = data[0].replace("#","").strip().split(";")
+        for i in data:
+            if i[0]!="#" and i!="":
+                objekty.append(i.strip()) 
+
+    for i in objekty:
+        if i.split(";")[0].lower() == char["rasa"]:
+            objekt = i.split(";")
+
+    for i, j in zip(keys, objekt):
+        opravy[i] = j
+
+    return opravy 
+def vyberNahodneZeSouboru(soubor):
     char = {}
     objekty = []
     with open(soubor, "r") as f:
@@ -37,10 +55,39 @@ Odolnost: {char["odolnost"]}
 Obratnost: {char["obratnost"]}
 Charisma: {char["charisma"]}""")
 
-char = vyberZeSouboru("classes")
-rasa = vyberZeSouboru("races")
-nahradNedostatky(char, rasa)
+def vygenerujStaty(char):
+    switcher = {5: 1, 10: 2, 15: 3}
+    staty = ["sila","obratnost","odolnost","inteligence","charisma"]
+    opravy = najdiOpravu("tabOprav", char)
+    for i in staty:
+        meze = char[i].split("-")
+        pocetHodu = int(meze[1]) - int(meze[0])
+        pocetHodu = switcher[pocetHodu]
+        bonus = int(meze[0]) - pocetHodu 
+        hod = 0
 
-with open("names", "r") as f:
-    char["jmeno"] = random.choice(f.readlines()).strip()
-vypisCharakteru(char)
+        for j in range(pocetHodu):
+            hod += random.randint(1,6)
+            print(f"Hodil si {hod} pro {i}")
+
+        char[i] = hod + bonus
+        print(f"Rasová oprava pro {i}: {char[i]} + {opravy[i]}")
+        char[i] = char[i] + int(opravy[i])
+
+if __name__ == "__main__":        
+    char = vyberNahodneZeSouboru("povolani")
+    rasa = vyberNahodneZeSouboru("rasy")
+    nahradNedostatky(char, rasa)
+
+    with open("jmena", "r") as f:
+        char["jmeno"] = random.choice(f.readlines()).strip()
+
+    print("Základ\n===")
+    vypisCharakteru(char)
+    print("===")
+
+    vygenerujStaty(char)
+    print("===\nVypočtené staty\n===")
+    vypisCharakteru(char)
+
+
